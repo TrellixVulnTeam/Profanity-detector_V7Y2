923b52e045193bb934f9ed2e9489db721bf179c8
Nilesh Pandey <32990329+nile649@users.noreply.github.com>
2021-08-14 17:30:56 -0500

    Delete ParentalControl/keywordYT directory

--- ParentalControl/keywordYT/__init__.py
+++
@@ -1,3 +0,0 @@
-# The code for the model and the trained model is taken from https://github.com/vzhou842/profanity-check
-# Model is trained using SVM.
-from .profanity import predict, predict_prob, YoutubeProfanity

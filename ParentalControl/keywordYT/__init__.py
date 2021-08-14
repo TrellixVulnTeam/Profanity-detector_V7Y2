f9acc007d4ccde4981e6439ea1625e1bd0b70952
Nilesh Pandey <32990329+nile649@users.noreply.github.com>
2021-08-14 17:30:18 -0500

    Add files via upload

---
+++ ParentalControl/keywordYT/__init__.py
@@ -0,0 +1,3 @@
+# The code for the model and the trained model is taken from https://github.com/vzhou842/profanity-check
+# Model is trained using SVM.
+from .profanity import predict, predict_prob, YoutubeProfanity
